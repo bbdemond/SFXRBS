ec00a4f665ac4198394b91b3daa4dcffd335c1f0
bbdemond <76221758+bbdemond@users.noreply.github.com>
2024-12-20 01:57:56 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,58 @@
+// JavaScript source code
+const modal = document.getElementById("my-modal");
+const span = document.getElementsByClassName("close")[0];
+modal.addEventListener("submit", sendForm)
+
+document.querySelectorAll('.openModalBtn').forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+
+function handleClick() {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
+
+function validatePhoneNumber(phoneNumber) {
+    const reRU = /^(\+7|7|8)?[\s-]?\(?\d{3}\)?[\s-]?\d{3}[\s-]?\d{2}[\s-]?\d{2}$/;
+    const reUS = /^(?:\+1|1)?\s?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
+    const reEN = /^(\+44|0)?\s?\(?\d{4}\)?[\s.-]?\d{3}[\s.-]?\d{3}$/;
+    const reGER = /^(\+49|0)?\s?\(?\d{3,4}\)?[\s.-]?\d{3,4}[\s.-]?\d{4}$/;
+    return reRU.test(phoneNumber) || reUS.test(phoneNumber) || reEN.test(phoneNumber) || reGER.test(phoneNumber);
+}
+
+function sendForm(event) {
+    var form = document.getElementById("my-form");
+    event.preventDefault();
+    var name = form.name.value;
+    var email = form.email.value;
+    var phone = form.phone.value;
+
+    if (name.trim().split(" ").length < 3) {
+        alert("Enter correct name");
+    }
+    else if (!validateEmail(email)) {
+        alert("Enter correct email");
+    }
+    else if (!validatePhoneNumber(phone)) {
+        alert("Enter correct phone number");
+    }
+    else {
+        modal.style.display = "none";
+        form.reset();
+        alert("SUCCESS");
+    }
+}
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
